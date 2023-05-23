c8119875db9159b05dc33183f926cbb4ef02d879
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-05-23 21:41:32 +0545

    Doing Sir

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
-
+import * as fs from 'fs';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
@@ -11,8 +11,28 @@ app.use(express.static(__dirname + '/public'));
 app.get("/", function (req, res) {
     res.render("index");
 });
-app.get("/event", function (req, res) {
-    res.render("SingleEvent");
+
+app.get("/event/:name", function (req, res) {
+    var name = req.params.name;
+
+    fs.readFile("MajorEvents.json", 'utf-8', (error, data) => {
+        if (error) {
+            return res.render("error");
+        } else {
+            const parsedData = JSON.parse(data);
+            var  eventData = parsedData.filter((curr) => {
+                if (curr.id == name.toString()) {
+                    return curr
+                }
+            });
+            if (eventData.length == 0) {
+                return res.render("error");
+
+            } else {
+                return res.render('SingleEvent',{ data: eventData[0] })
+            }
+        }
+    });
 });
 app.get("/*", function (req, res) {
     res.render("error");
