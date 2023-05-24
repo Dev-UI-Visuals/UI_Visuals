7ba25f78e145af92d00b507af78e07a1fee466ed
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-05-24 16:10:31 +0545

    Added Member and Leader JSON

--- index.js
+++ index.js
@@ -8,19 +8,27 @@ const app = express();
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public'));
 
-app.get("/", function (req, res) {
-    res.render("index");
+app.get("/", async function (req, res) {
+    var allData = { leaders: [], members: [] };
+    try{
+        allData.leaders = JSON.parse( await fs.promises.readFile(("data/Leader.json"),'utf-8'));
+        allData.members = JSON.parse( await fs.promises.readFile(("data/Member.json"),'utf-8'));
+        res.render("index", { data: allData });
+    }catch(e){
+        return res.render("error");
+    }
+
 });
 
 app.get("/event/:name", function (req, res) {
     var name = req.params.name;
 
-    fs.readFile("MajorEvents.json", 'utf-8', (error, data) => {
+    fs.readFile("data/MajorEvents.json", 'utf-8', (error, data) => {
         if (error) {
             return res.render("error");
         } else {
             const parsedData = JSON.parse(data);
-            var  eventData = parsedData.filter((curr) => {
+            var eventData = parsedData.filter((curr) => {
                 if (curr.id == name.toString()) {
                     return curr
                 }
@@ -29,7 +37,7 @@ app.get("/event/:name", function (req, res) {
                 return res.render("error");
 
             } else {
-                return res.render('SingleEvent',{ data: eventData[0] })
+                return res.render('SingleEvent', { data: eventData[0] })
             }
         }
     });
