e53a9c0c6b8705abc8792853c02f615f8843a01b
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-06-12 21:13:12 +0545

    Working on View More Event

--- public/js/script.js
+++ public/js/script.js
@@ -3,44 +3,92 @@ gsap.registerPlugin(ScrollToPlugin);
 
 var moreEvents = [
     {
-        "profileImage": "/image/events/UPC_Design_Hunt.png",
+        "id": "herald-code-brisk-hackathon",
+        "name": "Herald Code Brisk Hackathon",
+        "type":"Major Event",
+        "location": "Kuri Village, Kalinchowk",
+        "date": "2023-04-23",
+        "headerImage": "https://heraldcollege.edu.np/storage/post/HCK-Code-Brisk-post_1676978748.jpg",
+        "profileImage": "/image/events/Code_Brisk.png",
+        "description": [
+            "Herald DevCorps UI Visual Community, a student-led community at Herald College Kathmandu, successfully organized the nonstop 24-hour 'Code Brisk Hackathon 2023', at Kuri Village Kalinchowk. Situated at an altitude of 3447m above sea level, Code Brisk marks the hackathon held at the highest altitude. A total of 13 colleges participated in the hackathon, with three outstanding teams chosen to construct their prototypes at Kalinchowk. The primary theme of this competition was to boost the tourism and hospitality sector of Kalinchowk through innovative and practical prototypes created by the top three teams.",
+            "'Our community's unwavering effort and dedication to this project is commendable and has resulted in an incredible outcome. It is a testament to the passion and talent within our community, and I am proud of every one of you', says Mr. Prakash Shrestha, the CEO of Herald College Kathmandu, expressing his gratitude towards the organizing team.Herald College Kathmandu congratulates the winning team, Virinchi College - Team Everest for their revolutionary project, 'Tidy Town', which employs sensors to tackle waste management issues and offers a practical way of waste disposal. We would also like to extend our appreciation to all the participating colleges. Ms. Sujata Thapa, Head of Herald Devcorps, also praised the organizing committee and the top three colleges, “The effort of everyone in our community is reflected in the incredible work accomplished, and I am honored to be a part of it. Additionally, the top 3 teams presented truly impressive and achievable ideas, leaving us amazed."
+        ],
+        "youtubeCode": "50aIeMJ5xyQ",
+        "images": [
+            "/image/events/Code_Brisk_1.png",
+            "/image/events/Code_Brisk_1.png",
+            "/image/events/Code_Brisk_1.png",
+            "/image/events/Code_Brisk_1.png"
+        ]
+    },
+    {
+        "id": "upc-design-hunt",
         "name": "UPC Design Hunt",
-        "description": "UI Visuals organized an exciting event called Design Hunt, aimed at providing a platform for students to showcase their creativity and design skills. The event focused on designing a mobile app with unique features for new UPC early birds. The event was executed successfully, thanks to the efforts and support of our members. The event aimed to challenge participants' creativity and problem-solving skills while promoting collaboration and teamwork. Participants were given the resources to design a mobile app with unique features for new UPC early birds, showcasing their skills and potential in the UI and Visual Design field. The event encouraged participants to explore their creativity and learn from their peers and community leaders.",
+        "type":"Minor Event",
         "location": "Herald College, Naxal",
         "date": "2023-02-09",
-        "headerImage": "/image/events/UPC_Design_Hunt_1.png"
+        "headerImage": "/image/events/UPC_Design_Hunt_1.png",
+        "profileImage": "/image/events/UPC_Design_Hunt.png",
+        "description": [
+            "UI Visuals organized an exciting event called Design Hunt, aimed at providing a platform for students to showcase their creativity and design skills. The event focused on designing a mobile app with unique features for new UPC early birds. The event was executed successfully, thanks to the efforts and support of our members. The event aimed to challenge participants' creativity and problem-solving skills while promoting collaboration and teamwork. Participants were given the resources to design a mobile app with unique features for new UPC early birds, showcasing their skills and potential in the UI and Visual Design field. The event encouraged participants to explore their creativity and learn from their peers and community leaders."
+        ],
+        "youtubeCode": null,
+        "images": []
     },
     {
-        "profileImage": "/image/events/Vision_Quest.png",
+        "id": "vision-quest",
         "name": "Vision Quest",
-        "description": "UI Visuals organized a remarkable event to help +2 students interested in the field of IT. The event named Vision Quest: Sneak Peek into the Design World was organized to give aspiring software developers and designers a first-hand experience of the design process. The event was specially designed for +2 students who were looking forward to pursuing a career in the field of IT. The event focused on two key topics, namely 'Software Development' and 'UI/UX Design'. Both of these topics are critical to the IT industry, and understanding them is essential for anyone looking to make a career in the field. The event was curated by the community leaders who had years of experience in these fields and had a lot to share with the participants. The event was a great success, with many students attending and learning a lot about software development and UI/UX design. The participants were enthusiastic, and the experts' presentations were engaging and informative. Overall, the event was an excellent opportunity for +2 students to learn about the IT industry and gain valuable insights into software development and UI/UX design.",
+        "type":"Minor Event",
         "location": "Herald College, Naxal",
         "date": "2022-09-01",
-        "headerImage": "/image/events/Vision_Quest_1.png"
+        "headerImage": "/image/events/Vision_Quest_1.png",
+        "profileImage": "/image/events/Vision_Quest.png",
+        "description": [
+            "UI Visuals organized a remarkable event to help +2 students interested in the field of IT. The event named Vision Quest: Sneak Peek into the Design World was organized to give aspiring software developers and designers a first-hand experience of the design process. The event was specially designed for +2 students who were looking forward to pursuing a career in the field of IT. The event focused on two key topics, namely 'Software Development' and 'UI/UX Design'. Both of these topics are critical to the IT industry, and understanding them is essential for anyone looking to make a career in the field. The event was curated by the community leaders who had years of experience in these fields and had a lot to share with the participants. The event was a great success, with many students attending and learning a lot about software development and UI/UX design. The participants were enthusiastic, and the experts' presentations were engaging and informative. Overall, the event was an excellent opportunity for +2 students to learn about the IT industry and gain valuable insights into software development and UI/UX design."
+        ],
+        "youtubeCode": null,
+        "images": []
     },
     {
-        "profileImage": "/image/events/Photoshop_Session.png",
+        "id": "photoshop-session",
         "name": "Photoshop Session",
-        "description": "UI Visuals successfully organized a session on Photoshop Session for beginners. The event provided an opportunity for participants to learn how to use Photoshop tools to edit and manipulate images, as well as design minimalistic posters. The session was beneficial for students who are starting their journey in design and editing, as well as those who want to become familiar with other Adobe software in the future. Participants were able to book their seats by filling out a form and joining the event. UI Visuals hopes that the knowledge and skills gained from this event will be useful for participants in their future design endeavors.",
-        "location": "Herald College, Naxal",
+        "type":"Minor Event",
         "date": "2022-04-12",
-        "headerImage": "/image/events/Photoshop_Session_1.png"
-    },
-    {
-        "profileImage": "/image/events/Photography_Session.png",
-        "name": "Photography Session",
-        "headerImage": "/image/Events/Photography_Session_1.png",
-        "description": "UI Visuals organized a photography session as a part of the Visual Mela event in the Herald Devfest 2021. The session featured guest speakers Mr. Oscar Shrestha, a professional photographer and Islington graduate, and Mr. Pratik Pradhan and Mr. Rakshak Bhusan Bajracharya as guest lecturers. The session was held at Kumari Hall, Kamalpokhari. Participants had the opportunity to learn about photography concepts, creativity, dos and don'ts while clicking photos, and real-life experiences from the guest speakers.Additionally, the guest tutors provided professional ideas on how to click better photos and also provided feedback on the photographs submitted by the participants on that very day. Overall, the session was a great opportunity for photography enthusiasts to learn from industry professionals and improve their photography skill.",
-        "date": "2021-11-01",
-        "location": "Kumari Hall"
-    },
-    {
-        "profileImage": "/image/events/Figma_Session.png",
-        "headerImage": "/image/events/Figma_Session_1.png",
-        "name": "Figma Session",
-        "description": "UI Visuals organized a Figma Session led by our community leader Ms. Nerisha Shrestha as a part of their Visual Mela 2021. This session was targeted towards beginners who are looking for guidance and a starting point for their design journey. Figma is a user-friendly web-based/desktop app that is useful for graphics designing and prototyping. It generates the code based on the design and can be used for wireframing websites, designing mobile app interfaces, prototyping, and creating social media posts.The session took place from today until the 30th of September, and participants were encouraged to bring their laptops to get hands-on experience with Figma tools and resources. UI Visuals was excited to share their knowledge and experience about interface designing with attendees. This Figma Session was a great opportunity for anyone looking to learn more about design, and it was a part of the series of events organized for the Visual Mela 2021.",
         "location": "Herald College, Naxal",
-        "date": "2021-09-28"
+        "headerImage": "/image/events/Photoshop_Session_1.png",
+        "profileImage": "/image/events/Photoshop_Session.png",
+        "description": "UI Visuals successfully organized a session on Photoshop Session for beginners. The event provided an opportunity for participants to learn how to use Photoshop tools to edit and manipulate images, as well as design minimalistic posters. The session was beneficial for students who are starting their journey in design and editing, as well as those who want to become familiar with other Adobe software in the future. Participants were able to book their seats by filling out a form and joining the event. UI Visuals hopes that the knowledge and skills gained from this event will be useful for participants in their future design endeavors.",
+        "youtubeCode": null,
+        "images": []
+    },
+    {
+        "id": "photography-session",
+        "name": "Photography Session",
+        "type":"Minor Event",
+        "date": "2021-11-01",
+        "location": "Kumari Hall",
+        "headerImage": "/image/Events/Photography_Session_1.png",
+        "profileImage": "/image/events/Photography_Session.png",
+        "description": [
+            "UI Visuals organized a photography session as a part of the Visual Mela event in the Herald Devfest 2021. The session featured guest speakers Mr. Oscar Shrestha, a professional photographer and Islington graduate, and Mr. Pratik Pradhan and Mr. Rakshak Bhusan Bajracharya as guest lecturers. The session was held at Kumari Hall, Kamalpokhari. Participants had the opportunity to learn about photography concepts, creativity, dos and don'ts while clicking photos, and real-life experiences from the guest speakers.Additionally, the guest tutors provided professional ideas on how to click better photos and also provided feedback on the photographs submitted by the participants on that very day. Overall, the session was a great opportunity for photography enthusiasts to learn from industry professionals and improve their photography skill."
+        ],
+        "youtubeCode": null,
+        "images": []
+    },
+    {
+        "id":"figma-session",
+        "name": "Figma Session",
+        "type":"Minor Event",
+        "date": "2021-09-28",
+        "location": "Herald College, Naxal",
+        "headerImage": "/image/events/Figma_Session_1.png",
+        "profileImage": "/image/events/Figma_Session.png",
+        "description": [
+            "UI Visuals organized a Figma Session led by our community leader Ms. Nerisha Shrestha as a part of their Visual Mela 2021. This session was targeted towards beginners who are looking for guidance and a starting point for their design journey. Figma is a user-friendly web-based/desktop app that is useful for graphics designing and prototyping. It generates the code based on the design and can be used for wireframing websites, designing mobile app interfaces, prototyping, and creating social media posts.The session took place from today until the 30th of September, and participants were encouraged to bring their laptops to get hands-on experience with Figma tools and resources. UI Visuals was excited to share their knowledge and experience about interface designing with attendees. This Figma Session was a great opportunity for anyone looking to learn more about design, and it was a part of the series of events organized for the Visual Mela 2021."
+        ],
+        "youtubeCode": null,
+        "images": []
     }
 ]
 
@@ -58,14 +106,16 @@ setButton(true);
 function seeMoreEvents(isMore) {
     var EventsElement = document.getElementById("events");
     if (isMore) {
+        console.log(moreEvents);
         moreEvents.forEach((curr, indx) => {
             if (indx > 2) {
                 EventsElement.innerHTML += `<div class="single-event flex-col">
             <div class="event-title">
-              <h3>${curr.name}</h3>
+              <h3>${curr.type}</h3>
             </div>
             <img src="${curr.profileImage}" />
             <div class="single-event-content flex-col">
+            <h3>${curr.name}</h3>
               <div class="flex-row events-icons">
                 <i class="uil uil-location-point"></i>
                 <h5 style="font-size: 14px">${curr.location}</h5>
